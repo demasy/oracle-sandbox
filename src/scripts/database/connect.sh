34b2cba2c3ae053bbe0be1190708406ad513ba42
Ahmed El-Demasy <39886429+demasy@users.noreply.github.com>
2025-07-22 01:39:04 +0300

    Add .dockerignore file

--- src/scripts/database/connect.sh
+++ src/scripts/database/connect.sh
@@ -6,8 +6,8 @@ normal=$(tput sgr0)
 
 echo ""
 echo "  |--------------------------------------------------------|"
-echo -e "  |            \e[1;31m     Demasy Labs Database V1.0  \e[0m            |"
-echo -e "  |                \e[1;33m Developed by Demasy Labs\e[0m               |"
+echo -e "  |          \e[1;31m     Demasy Labs Database V1.0  \e[0m              |"
+echo -e "  |             \e[1;33m Developed by Demasy Labs\e[0m                  |"
 echo "  |                email: founder@demasy.io                |"
 echo "  |                website: www.demasy.io                  |"
 echo "  |             github: www.github.com/demasy              |"
@@ -16,20 +16,20 @@ echo "  |--------------------------------------------------------|"
 echo ""
 echo "      Welcome! $USER - $(date)"
 echo ""
-echo "       - Host: $ROKETTO_DB_HOST"
-echo "       - Port: $ROKETTO_DB_PORT"
-echo "       - Service: $ROKETTO_DB_SERVICE"
-echo "       - User: $ROKETTO_DB_USER"
+echo "       - Host: $DEMASYLABS_DB_HOST"
+echo "       - Port: $DEMASYLABS_DB_PORT"
+echo "       - Service: $DEMASYLABS_DB_SERVICE"
+echo "       - User: $DEMASYLABS_DB_USER"
 echo ""
 
 # Check required environment variables
-if [[ -z "$ROKETTO_DB_HOST" || -z "$ROKETTO_DB_PORT" || -z "$ROKETTO_DB_SERVICE" || -z "$ROKETTO_DB_USER" || -z "$ROKETTO_DB_PASS" ]]; then
+if [[ -z "$DEMASYLABS_DB_HOST" || -z "$DEMASYLABS_DB_PORT" || -z "$DEMASYLABS_DB_SERVICE" || -z "$DEMASYLABS_DB_USER" || -z "$DEMASYLABS_DB_PASS" ]]; then
   echo "Error: One or more required environment variables are not set."
   exit 1
 fi
 
 # Connect to Oracle
-sql "$ROKETTO_DB_USER/$ROKETTO_DB_PASS@$ROKETTO_DB_HOST:$ROKETTO_DB_PORT/$ROKETTO_DB_SERVICE" || {
+sql "$DEMASYLABS_DB_USER/$DEMASYLABS_DB_PASS@$DEMASYLABS_DB_HOST:$DEMASYLABS_DB_PORT/$DEMASYLABS_DB_SERVICE" || {
   echo "Failed to connect to Oracle Database."
   exit 2
 }
